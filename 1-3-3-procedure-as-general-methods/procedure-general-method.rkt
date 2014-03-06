22337490c87d0701b4c6dfb755e8cd65f40166c6
曹家锋 <funfriendcjf@gmail.com>
2014-03-06 20:54:22 +0800

    root of equation and fixed point of function

---
+++ 1-3-3-procedure-as-general-methods/procedure-general-method.rkt
@@ -0,0 +1,49 @@
+#lang planet neil/sicp
+
+;;; half-interval method to find roots of equation f(x)=0 where
+;;; is a continuous function.
+
+(define (search f neg-point pos-point)
+  (define (average x y) (/ (+ x y) 2))
+  (define (close-enough? x y) (< (abs (- x y)) 0.001))
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value) (search f neg-point midpoint))
+                ((negative? test-value) (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value)) (search f a b))
+          ((and (negative? b-value) (positive? a-value)) (search f b a))
+          (else (error "Value are not of opposite sign" a b)))))
+
+; (half-interval-method sin 2.0 4.0)
+; (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3)) 1.0 2.0)
+
+
+;;; fixed point of functions
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? x y) (< (abs (- x y)) tolerance))
+  (define (try guess)
+    (let ((next-guess (f guess)))
+      (if (close-enough? next-guess guess)
+          next-guess
+          (try next-guess))))
+  (try first-guess))
+
+; (fixed-point cos 1.0)
+; (fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+
+
+;;; sqrt ...
+;;; average damping
+(define (sqrt x)
+  (define (average x y) (/ (+ x y) 2.0))
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
