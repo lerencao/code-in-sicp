1d74f01b96f50087535a5793c08118b8b4618e4d
曹家锋 <funfriendcjf@gmail.com>
2014-03-17 17:20:13 +0800

    operations on list
    ref, length, append

--- 2-2-hierarchical-data-and-the-closure-property.rkt
+++ 2-2-hierarchical-data-and-the-closure-property.rkt
@@ -16,3 +16,25 @@
 (car one-four)
 (cdr one-four)
 (cons 10 one-four)
+
+;;; list operations
+(define (list-ref ls n)
+  (if (= n 0)
+      (car ls)
+      (list-ref (cdr ls) (- n 1)))) ; (list-ref one-four 2)
+
+(define (length ls)
+  (if (null? ls)
+      0
+      (+ 1 (length (cdr ls))))) ;(length '(1 3 5 7))
+(define (length-iter ls)
+  (define (iter a count)
+    (if (null? a)
+        count
+        (iter (cdr a) (+ count 1))))
+  (iter ls 0))
+
+(define (append ls1 ls2)
+  (if (null? ls1)
+      ls2
+      (cons (car ls1) (append (cdr ls1) ls2))))
