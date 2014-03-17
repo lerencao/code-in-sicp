5256539e648d9c607a558572af5d50016f5fba29
曹家锋 <funfriendcjf@gmail.com>
2014-03-17 18:46:55 +0800

    solve exercise 2.25

--- 2-2-hierarchical-data-and-the-closure-property.rkt
+++ 2-2-hierarchical-data-and-the-closure-property.rkt
@@ -110,3 +110,13 @@
         (else (+ (count-leaves (car ls))
                  (count-leaves (cdr ls))))))
 ; (count-leaves (list 1 2 (list 2 3)))
+
+;; exercise 2.25
+(car (cdr (car (cdr (cdr '(1 3 (5 7) 9))))))
+(car (car '((7))))
+(car (cdr
+      (car (cdr
+            (car (cdr
+                  (car (cdr
+                        (car (cdr
+                              (car (cdr '(1 (2 (3 (4 (5 (6 7))))))))))))))))))
