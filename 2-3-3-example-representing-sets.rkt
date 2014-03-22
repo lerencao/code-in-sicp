d41a9e9ff65440e1b1a037a390948dc8813db78d
曹家锋 <funfriendcjf@gmail.com>
2014-03-22 23:17:15 +0800

    example: representing set.

---
+++ 2-3-3-example-representing-sets.rkt
@@ -0,0 +1,9 @@
+#lang planet neil/sicp
+
+;;; representing sets
+
+; operations on sets
+(define (union-set seta setb))
+(define (intersection-set seta setb))
+(define (element-of-set? e set))
+(define (adjoin-set e set))
