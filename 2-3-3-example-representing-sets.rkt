83e263b1d2e519bcb59aac16cd8c68bb7fb31af9
曹家锋 <funfriendcjf@gmail.com>
2014-03-22 23:48:50 +0800

    using unordered lists to represent sets

--- 2-3-3-example-representing-sets.rkt
+++ 2-3-3-example-representing-sets.rkt
@@ -3,7 +3,19 @@
 ;;; representing sets
 
 ; operations on sets
-(define (union-set seta setb))
-(define (intersection-set seta setb))
-(define (element-of-set? e set))
-(define (adjoin-set e set))
+(define (union-set seta setb)
+  (cond ((null? seta) setb)
+        ((element-of-set? (car seta) setb)
+         (union-set (cdr seta) setb))
+        (else (cons (car seta) (union-set (cdr seta) setb)))))
+(define (intersection-set seta setb)
+  (cond ((null? seta) nil)
+        ((element-of-set? (car seta) setb)
+         (cons (car seta) (intersection-set (cdr seta) setb)))
+        (else (intersection-set (cdr seta) setb))))
+(define (element-of-set? e set)
+  (cond ((null? set) false)
+        ((equal? e (car set)) true)
+        (else (element-of-set? e (cdr set)))))
+(define (adjoin-set e set)
+  (if (element-of-set? e set) set (cons e set)))
