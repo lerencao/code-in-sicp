fcec8da21a171021722a3566edc6ec122900d26a
曹家锋 <funfriendcjf@gmail.com>
2014-03-23 22:01:24 +0800

    solve exercise 2.64

--- 2-3-3-example-representing-sets.rkt
+++ 2-3-3-example-representing-sets.rkt
@@ -96,3 +96,25 @@
 ; the two procedures produce the same result. (a)
 ; list for figure 2.16 is (1 3 5 7 9 11)
 ; and they have same order of growth. (b)
+
+; exercise 2.64
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0) (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts) right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+; a
+; select first (n-1)/2 elems to construct left tree,
+; use the left to construct entry and right tree.
+
+; b, lg(n)
