f98e50ad303a929e90678f4d1f08b88ccd571c8c
曹家锋 <funfriendcjf@gmail.com>
2014-03-23 10:30:23 +0800

    use ordered list to represent set

--- 2-3-3-example-representing-sets.rkt
+++ 2-3-3-example-representing-sets.rkt
@@ -28,3 +28,32 @@
 ; multiset can be used here for duplicate elements.
 ; application: the multiset of prime factors of a number n.
 ; for number 120, the result is {2, 2, 2, 3, 5}
+
+; use ordered list to represent set
+(define (element-of-set? e set)
+  (cond ((null? set) false)
+        ((= e (car set)) true)
+        ((< e (car set)) false)
+        (else (element-of-set? e (cdr set)))))
+(define (intersection-set seta setb)
+  (if (or (null? seta) (null? setb)) nil
+      (let ((x1 (car seta))
+            (x2 (car setb)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr seta) (cdr setb))))
+              ((< x1 x2) (intersection-set (cdr seta) setb))
+              ((> x1 x2) (intersection-set seta (cdr setb)))))))
+(define (adjoin-set e set)
+  (cond ((null? set) (list e))
+        ((= e (car set)) set)
+        ((< e (car set)) (cons e set))
+        (else (cons (car set) (adjoin-set e (cdr set))))))
+(define (union-set seta setb)
+  (cond ((null? seta) setb)
+        ((null? setb) seta)
+        (else
+         (let ((x1 (car seta)) (x2 (car setb)))
+           (cond ((= x1 x2)
+                  (cons x1 (union-set (cdr seta) (cdr setb))))
+                 ((< x1 x2) (cons x1 (union-set (cdr seta) setb)))
+                 (else (cons x2 (union-set seta (cdr setb)))))))))
