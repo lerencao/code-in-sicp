4d0580de6859a8f85e5bce87af2fd1b7c1f550cf
曹家锋 <funfriendcjf@gmail.com>
2014-03-23 21:06:31 +0800

    use binary tree yo representing trees

--- 2-3-3-example-representing-sets.rkt
+++ 2-3-3-example-representing-sets.rkt
@@ -57,3 +57,25 @@
                   (cons x1 (union-set (cdr seta) (cdr setb))))
                  ((< x1 x2) (cons x1 (union-set (cdr seta) setb)))
                  (else (cons x2 (union-set seta (cdr setb)))))))))
+
+; sets as binary trees
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right) (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= (entry set) x) true)
+        ((< (entry set) x) (element-of-sets? x (right-branch set)))
+        ((> (entry set) x) (element-of-sets? x (left-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set)) (make-tree (entry set)
+                                      (adjoin-set x (left-branch set))
+                                      (right-branch set)))
+        ((> x (entry set)) (make-tree (entry set)
+                                     (left-branch set)
+                                     (adjoin-set x (right-branch set))))))
