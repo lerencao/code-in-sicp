28e4b95cd265d4d12609c96089408741968dca58
曹家锋 <funfriendcjf@gmail.com>
2014-03-24 16:43:32 +0800

    solve exercise 2.67

--- 2-3-4-example-huffman-encoding-trees.rkt
+++ 2-3-4-example-huffman-encoding-trees.rkt
@@ -46,3 +46,12 @@
       (let ((pair (car pairs)))
         (adjoin-set (make-leaf (car pair) (cadr pair))
                     (make-leaf-set (cdr pairs))))))
+
+; exercise 2.67
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree (make-leaf 'B 2)
+                                  (make-code-tree (make-leaf 'D 1)
+                                                  (make-leaf 'C 1)))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+; the result is (A D A B B C A)
