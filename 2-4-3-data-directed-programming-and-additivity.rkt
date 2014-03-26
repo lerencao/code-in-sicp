ec1d0b7deb1ca4536a63156afd794b90be28ebda
Cao Jiafeng <funfriendcjf@gmail.com>
2014-03-26 10:00:00 +0800

    what is the message passing

--- 2-4-3-data-directed-programming-and-additivity.rkt
+++ 2-4-3-data-directed-programming-and-additivity.rkt
@@ -129,3 +129,32 @@
 
 (define (get-salary employee-record)
   ((get 'get-salary (car employee-record)) (cadr employee-record)))
+
+;; Message passing
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude) (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else (error "Unkown op: make-from-real-imag" op))))
+  dispatch)
+; the value returned by make-from-real-imag is a procedure.
+(define (apply-generic op arg) (arg op))
+
+; exercise 2.75
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          (else (error "Unkown op: make-from-mag-ang" op))))
+  dispatch)
+
+; exercise 2.76
+
+; select generic operations with explicit dispatch
+; when new operations must often be added.
+
+; and select Message passing when new types must often be added.
