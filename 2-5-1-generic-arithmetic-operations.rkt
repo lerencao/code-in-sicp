5f664fa180d09675d1c903437044a29d93cfeb89
Cao Jiafeng <funfriendcjf@gmail.com>
2014-03-27 11:56:29 +0800

    make the arithmetic operation procedures runnable
    the procedure get and put have been implemented.

--- 2-5-1-generic-arithmetic-operations.rkt
+++ 2-5-1-generic-arithmetic-operations.rkt
@@ -1,4 +1,30 @@
-#lang planet neil/sicp
+#lang racket
+
+(define table (make-hash))
+(define (put op type-tags proc)
+  (hash-set! table (cons op type-tags) proc))
+(define (get op type-tags)
+  (hash-ref table (cons op type-tags) false))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these typs: APPLY-GENERIC"
+                 (list op type-tags))))))
+
 
 ; generic arithmetic procedures
 (define (add x y) (apply-generic 'add x y))
@@ -66,7 +92,62 @@
 
 ; package handling complex numbers
 (define (install-complex-package)
+  ;; complex number representations
+  (define (install-rectangular-package)
+    ;; internal procedures
+    (define (square x) (expt x 2))
+    (define (real-part z) (car z))
+    (define (imag-part z) (cdr z))
+    (define (magnitude z)
+      (sqrt (+ (square (real-part z))
+               (square (imag-part z)))))
+    (define (angle z)
+      (atan (imag-part z) (real-part z)))
+    (define (make-from-real-imag x y) (cons x y))
+    (define (make-from-mag-ang r a) (cons (* r (cos a)) (* r (sin a))))
+
+    ;; interface to the rest of system
+    (define (tag x) (attach-tag 'rectangular x))
+    (put 'real-part '(rectangular) real-part)
+    (put 'imag-part '(rectangular) imag-part)
+    (put 'magnitude '(rectangular) magnitude)
+    (put 'angle '(rectangular) angle)
+    (put 'make-from-real-imag 'rectangular
+         (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'rectangular
+         (lambda (r a) (tag (make-from-mag-ang r a))))
+    'done)
+
+  (define (install-polar-package)
+    ;; internal procedures
+    (define (square x) (expt x 2))
+    (define (magnitude z) (car z))
+    (define (angle z) (cdr  z))
+    (define (real-part z) (* (magnitude z) (cos (angle z))))
+    (define (imag-part z) (* (magnitude z) (sin (angle z))))
+    (define (make-from-real-imag x y)
+      (cons (sqrt (+ (square x) (square y)))
+            (atan y x)))
+    (define (make-from-mag-ang r a) (cons r a))
+
+    ;; interface to the rest of system
+    (define (tag x) (attach-tag 'polar x))
+    (put 'real-part '(polar) real-part)
+    (put 'imag-part '(polar) imag-part)
+    (put 'magnitude '(polar) magnitude)
+    (put 'angle '(polar) angle)
+    (put 'make-from-real-imag 'polar
+         (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'polar
+         (lambda (r a) (tag (make-from-mag-ang r a))))
+    'done)
+  (install-polar-package)
+  (install-rectangular-package)
   ;; imported procedures from rectangular and polar packages
+  (define (real-part z) (apply-generic 'real-part z))
+  (define (imag-part z) (apply-generic 'imag-part z))
+  (define (magnitude z) (apply-generic 'magnitude z))
+  (define (angle z) (apply-generic 'angle z))
   (define (make-from-real-imag x y)
     ((get 'make-from-real-imag 'rectangular) x y))
   (define (make-from-mag-ang r a)
@@ -104,3 +185,7 @@
   ((get 'make-from-real-imag 'complex) x y))
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
+
+(install-scheme-number-package)
+(install-rational-package)
+(install-complex-package)
