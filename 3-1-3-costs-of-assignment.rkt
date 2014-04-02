41530df622c785b8293bd75eada98103ec5d0614
Cao Jiafeng <funfriendcjf@gmail.com>
2014-04-02 20:12:07 +0800

    sample code of chapter 3.1.3

---
+++ 3-1-3-costs-of-assignment.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define (make-simplified-withdraw balance)
+  (lambda (amount)
+    (set! balance (- balance amount))
+    balance))
+(define W (make-simplified-withdraw 25))
+
+(define (make-decrementer balance)
+  (lambda (amount)
+    (- balance amount)))
+(define D (make-decrementer 25))
