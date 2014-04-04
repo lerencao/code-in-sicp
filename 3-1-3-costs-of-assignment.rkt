1aa3830c547fcef059f19693d5b0c3629f87087f
Cao Jiafeng <funfriendcjf@gmail.com>
2014-04-04 21:03:03 +0800

    solve exercise 3.7, 3.8

--- 3-1-3-costs-of-assignment.rkt
+++ 3-1-3-costs-of-assignment.rkt
@@ -10,3 +10,17 @@
   (lambda (amount)
     (- balance amount)))
 (define D (make-decrementer 25))
+
+
+;;; shared account
+(define (make-joint acc password new-password)
+  (lambda (pass m)
+    (if (eq? pass new-password)
+        (acc password m)
+        (error "Wrong password" pass))))
+
+;;; f
+(define (f n)
+  (let ((state 1))
+    (begin (set! state (* state n))
+           state)))
