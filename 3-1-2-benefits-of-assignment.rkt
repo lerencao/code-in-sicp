b92e1c24e3737e0266ccccb7cd33fb37c8d43fa4
Cao Jiafeng <funfriendcjf@gmail.com>
2014-04-04 18:49:00 +0800

    solve exercise 3.5

--- 3-1-2-benefits-of-assignment.rkt
+++ 3-1-2-benefits-of-assignment.rkt
@@ -1,15 +1,15 @@
 #lang racket
 
-(define rand
-  (let ((x random-init))
-    (lambda ()
-      (set! x (rand-update x))
-      x)))
+;; (define rand
+;;   (let ((x random-init))
+;;     (lambda ()
+;;       (set! x (rand-update x))
+;;       x)))
 
-(define (estimate-pi trials)
-  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
-(define (cesaro-test)
-  (= (gcd (rand) (rand)) 1))
+;; (define (estimate-pi trials)
+;;   (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+;; (define (cesaro-test)
+;;   (= (gcd (rand) (rand)) 1))
 
 (define (monte-carlo trials experiment)
   (define (iter trials-remaining trials-passed)
@@ -20,3 +20,20 @@
                  (+ trials-passed 1)))
           (else (iter (- trials-remaining 1) trials-passed))))
   (iter trials 0))
+
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (estimate-integral p x1 x2 y1 y2 trials)
+  (define (experiment) (p (random-in-range x1 x2)
+                          (random-in-range y1 y2)))
+  (let ((rate (monte-carlo trials experiment))
+        (area (* (- x2 x1) (- y2 y1))))
+    (* rate area)))
+
+(define (estimate-pi trials)
+  (estimate-integral (lambda (x y) (<= (+ (expt x 2) (expt y 2)) 1))
+                          -1 1 -1 1
+                          trials))
