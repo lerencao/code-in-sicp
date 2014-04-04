e0314355f02c213cfcbdac3b83f769e52dbb045f
Cao Jiafeng <funfriendcjf@gmail.com>
2014-04-04 16:01:51 +0800

    solve exercise 3.2

--- 3-1-1-local-state-variables.rkt
+++ 3-1-1-local-state-variables.rkt
@@ -36,3 +36,13 @@
     (lambda (num)
       (set! acc (+ acc num))
       acc)))
+
+
+;;; monitor
+(define (make-monitored proc)
+  (let ((called-times 0))
+    (lambda (arg)
+      (cond ((eq? arg 'how-many-calls?) called-times)
+             ((eq? arg 'reset-count) (set! called-times 0))
+             (else (begin (set! called-times (+ called-times 1))
+                          (proc arg)))))))
