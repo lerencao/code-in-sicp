392e0ddda43667e6fbdc1f02636eb61a29d09fc5
Cao Jiafeng <funfriendcjf@gmail.com>
2014-04-11 15:14:06 +0800

    two-dimensional tables

--- 3-3-3-representing-tables.rkt
+++ 3-3-3-representing-tables.rkt
@@ -2,19 +2,27 @@
 
 (define (make-table) (list '*table*))
 
-(define (lookup key table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (cdr record)
-        false)))
 (define (assoc key records)
   (cond ((null? records) false)
         ((equal? key (caar records))
          (car records))
         (else (assoc key (cdr records)))))
 
-(define (insert! key value table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (set-cdr! record value)
-        (set-cdr! table (cons (cons key value) (cdr table))))))
+(define (insert! key1 key2 value table)
+  (let ((subtable (assoc key1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key2 (cdr subtable))))
+          (if record
+              (set-cdr! record value)
+              (set-cdr! subtable (cons (cons key2 value) (cdr subtable)))))
+        (set-cdr! table
+                  (cons (list key1 (cons key2 value))
+                        (cdr table))))))
+
+
+(define (lookup key1 key2 table)
+  (let ((subtable (assoc key1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key2 (cdr subtable))))
+          (if record (cdr record) false))
+        false)))
