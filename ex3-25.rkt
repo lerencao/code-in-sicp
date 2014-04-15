ed7b9c234930933a8b5779bfc760fe0653675a75
Cao Jiafeng <funfriendcjf@gmail.com>
2014-04-15 16:08:31 +0800

    an improved version of exercise 3.25
    it do what you want,
    (k1 k2) separate from (k1 k2 ...)

--- ex3-25.rkt
+++ ex3-25.rkt
@@ -1,48 +1,47 @@
 #lang planet neil/sicp
 
-(define (make-table) (list '*table*))
+(define (make-table) '(*table* ()))
 
 (define (assoc key records)
-  (cond ((null? records) false)
-        ((equal? key (caar records))
-         (car records))
-        (else (assoc key (cdr records)))))
-(define (lookup key table)
-  (let ((record (assoc key (cdr table))))
+  (cond [(null? records) false]
+        [(equal? key (caar records)) (car records)]
+        [else (assoc key (cdr records))]))
+
+(define (look-up key table)
+  (let ((record (assoc key (cadr table))))
     (if record
-        (cdr record)
+        (let ((value (cddr record)))
+          (if (pair? value)
+              (car value)
+              false))
         false)))
+
 (define (insert! key value table)
-  (let ((record (assoc key (cdr table))))
+  (let ((record (assoc key (cadr table))))
     (if record
-        (set-cdr! record value)
-        (set-cdr! table
-                  (cons (cons key value) (cdr table))))))
+        (set-cdr! (cdr record) (list value))
+        (set-car! (cdr table) (cons (list key '() value) (cadr table))))))
+
+(define (lookup-multi key-list table)
+  (cond ((null? key-list) (error "lookup-multi called with empty list"))
+        ((null? (cdr key-list))
+         (look-up (car key-list) table))
+        (else
+         (let ((subtable (assoc (car key-list) (cadr table))))
+           (if subtable
+               (lookup-multi (cdr key-list) subtable)
+               false)))))
 
 (define (insert-multi! key-list value table)
-  (cond ((null? key-list)
-         (error "INSERT-MULTI! called with empty key list" key-list))
-        ((null? (cdr key-list))
-         (insert! (car key-list) value table))
-        (else
-         (let ((subtable (assoc (car key-list) (cdr table))))
+  (cond [(null? key-list)
+         (error "insert-multi! called with empty key list")]
+        [(null? (cdr key-list))
+         (insert! (car key-list) value table)]
+        [else
+         (let ((subtable (assoc (car key-list) (cadr table))))
            (if subtable
                (insert-multi! (cdr key-list) value subtable)
                (begin
-                 (set-cdr! table
-                           (cons (list (car key-list))
-                                 (cdr table)))
-                 (set! subtable (assoc (car key-list) (cdr table)))
-                 (insert-multi! (cdr key-list) value subtable)))))))
-
-(define (lookup-multi key-list table)
-  (cond ((null? key-list)
-         (error "LOOKUP called with empty key list" key-list))
-        ((null? (cdr key-list))
-         (lookup (car key-list) table))
-        (else
-         (let ((subtable (assoc (car key-list) (cdr table))))
-           (if (or (not subtable) ; cannot find the key
-                   (not (pair? (cdr subtable)))) ; or have no subtables for left keys
-               false
-               (lookup-multi (cdr key-list) subtable))))))
+                 (set-car! (cdr table) (cons (list (car key-list) '()) (cadr table)))
+                 (set! subtable (assoc (car key-list) (cadr table)))
+                 (insert-multi! (cdr key-list) value subtable))))]))
