64585ea8d1cf5c2847f374846d8e99804e07ea3d
Cao Jiafeng <funfriendcjf@gmail.com>
2014-04-21 21:28:31 +0800

    sample code of 3.5.1

---
+++ 3.5-stream.rkt
@@ -0,0 +1,70 @@
+#lang racket
+
+;;; stream are delayed lists
+(define (cons-stream x y) (cons x (delay y)))
+(define (stream-car s) (car s))
+(define (stream-cdr s) (force (cdr s)))
+
+(define the-empty-stream '())
+(define stream-null? null?)
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref s (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+
+;; the stream implementation in action
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+
+;; implementing delay and force
+(define (delay exp)
+  (memo-proc (lambda () exp)))
+(define (force delayed-obj)
+  (delayed-obj))
+
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
+
+; *** TODO exercise 3.50 ~ 3.52
